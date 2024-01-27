44e4e4fd53504e3586719d9eb367cf99d225908a
aryo faizi <aryofaizi@gmail.com>
2024-01-27 23:19:37 +0330

    settings file created

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """the game settings can be changed through this class"""
+    
+    def __init__(self):
+        """initialize attributes"""
+        self.screen_width = 700
+        self.screen_height = 1_000
