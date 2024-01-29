0c97f34fe153e6d3b40a3fb69d340deaac3ce044
aryo faizi <aryofaizi@gmail.com>
2024-01-29 22:47:27 +0330

    bullet settings initialized

--- settings.py
+++ settings.py
@@ -5,3 +5,8 @@ class Settings:
         """initialize attributes"""
         self.screen_width = 700
         self.screen_height = 1_000
+        
+        # bullets 
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (0, 0, 255)
