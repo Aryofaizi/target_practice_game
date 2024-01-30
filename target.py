59c3ff5ad42bb646d3015ea262b803a62eff47d0
aryo faizi <aryofaizi@gmail.com>
2024-01-30 11:13:21 +0330

    the move and check_edges methods implemented

--- target.py
+++ target.py
@@ -9,6 +9,7 @@ class Target:
         
         self.screen = game.screen
         self.screen_rect = self.screen.get_rect()
+        self.settings = game.settings
 
         self.width, self.height = 70, 70
         self.bg = (255, 0, 0)
@@ -16,8 +17,24 @@ class Target:
         
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.midright = self.screen_rect.midright
+    
+        self.y = float(self.rect.y)    
+
+        # direction
+        self.direction = self.settings.direction
+        # speed 
+        self.speed = self.settings.target_speed
         
+    def move(self):
+        """moves the ship up and down at a steady rate"""
+        self.y += self.speed * self.direction 
+        self.rect.y = self.y
         
+    def check_edges(self):
+        if self.rect.top <= self.screen_rect.top or (
+            self.rect.bottom >= self.screen_rect.bottom):
+            return True
+            
     def draw_target(self):
         """draw target rect on screen"""
         self.screen.fill(self.bg, self.rect)
